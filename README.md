b2097f508dee0abbda48508b2b8fc3dc33f69d12
Azrililham138 <azrililham0508@gmail.com>
2025-02-24 10:57:58 +0700

    Tugas1_praktikum

--- README.md
+++
@@ -1,7 +0,0 @@
-#Pemrograman Web 2
-
-##Repositori Pratikum Pemrograman Web 2
-
-- Nama : Azril Ilham Pramudya
-- NIM : 0110124070
-- Rombel : SI11
---
+++ variable_user.php
@@ -0,0 +1,11 @@
+<?php
+//Mendefinisikan variable
+$nama = 'Azril';
+$umur = 19;
+$berat = 59.01;
+
+echo 'Nama Saya ' . $nama;
+echo '<br/>Umur : ' . $umur . ' Tahun';
+echo '<br/>Berat : ' . $berat;
+
+?>
